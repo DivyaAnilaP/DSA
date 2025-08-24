8bc302e158bbfd4da40dfb78e6a31fa3f283b6ad
Divya Anila Pantangi <155723912+DivyaAnilaP@users.noreply.github.com>
2025-08-24 13:37:06 +0530

    Create largestNumber.py

---
+++ largestNumber.py
@@ -0,0 +1,9 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        nums=list(map(str,nums))
+        nums.sort(key=lambda x:x*10,reverse=True)
+        res="".join(nums)
+        if res[0]=="0":
+            return "0"
+        else:
+            return res
