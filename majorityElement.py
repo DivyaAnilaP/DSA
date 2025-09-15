7a3f5f6e86c1b17a3ff8e5c653c893a88dce1ae7
Divya Anila Pantangi <155723912+DivyaAnilaP@users.noreply.github.com>
2025-09-15 15:14:41 +0530

    Create majorityElement.py

---
+++ majorityElement.py
@@ -0,0 +1,9 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        cand=None
+        count=0
+        for num in nums:
+            if count==0:
+                cand=num
+            count+=1 if cand==num else -1
+        return cand
